5d4004d5820a2d0ce6142d7429fd36d6639f6f29
g. nicholas d'andrea <nick@gnidan.org>
2019-04-18 10:32:46 -0400

    Add ganache network

--- truffle.js
+++ truffle.js
@@ -5,5 +5,12 @@ module.exports = {
     solc: {
       version: "^0.5.7"
     }
+  },
+  networks: {
+    ganache: {
+      host: "127.0.0.1",
+      port: "7545",
+      network_id: "*"
+    }
   }
 };
